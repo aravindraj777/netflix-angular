069bb83bd05c8bffdea485a76109e1b12bbf9bb8
Aravind <aravindmunna240@gmail.com>
2023-11-11 19:36:27 +0530

    movie details completed

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
-@Component({
+@Component({ 
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+
+  constructor(private service:MovieApiServiceService,private router:ActivatedRoute){}
+
+  getMovieDetailsResult:any;
+  getMovieVideoResult:any;
+  getMovieCastResult:any
+
+  ngOnInit(): void {
+    let getParamId = this.router.snapshot.paramMap.get('id');
+    this.getMovie(getParamId);
+    this.getVideo(getParamId);
+    this.getMovieCast(getParamId)
+
+   console.log(getParamId,'paramId#')
+   
+  }
+
+  getMovie(id:any){
+    this.service.movieDetails(id).subscribe((result)=>{
+      console.log(result,"getMovieDetails#");
+      this.getMovieDetailsResult = result
+      
+    })
+  }
+
+  getVideo(id:any){
+    this.service.getMovieVideo(id).subscribe((result)=>{
+      console.log(result,"videoResult#")
+      result.results.forEach((element:any) => {
+        if(element.type=="Trailer"){
+          this.getMovieVideoResult = element.key
+        }
+      });
+     
+    });
+  }
+
+  getMovieCast(id:any){
+    this.service.getMovieCast(id).subscribe((result)=>{
+      console.log(result,"castResult#");
+      this.getMovieCastResult = result.cast;
+      
+    })
+  }
 
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -23,9 +23,63 @@ export class MovieApiServiceService {
     return this.http.get(`${this.baseurl}/trending/movie/day?api_key=${this.apikey}`);
   }
 
+  // searching movie
   searchMovie(data:any):Observable<any>{
     console.log(data,'movies#')
     return this.http.get(`${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`)
+  }
+
+
+  // getMovieDetails
+  movieDetails(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}?api_key=${this.apikey}`)
+
+  }
+  
+  // getVideo
+  getMovieVideo(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`)
+  }
+
+  //getMovieCast
+  getMovieCast(data:any):Observable<any>{
+    return this.http.get(`${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`)
+  }
+
+  // Action Movies
+  fetchActionMovies():Observable<any>{
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=28`);
+  }
+
+  // Adventure Movies
+  fetchAdventureMovies():Observable<any>{
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=12`);
+  }
+
+  // Animation Movies
+  fetchAnimationMovies():Observable<any>{
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=16`);
+  }
+
+
+  // Comedy Movies
+  fetchComedyMovies():Observable<any>{
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=35`);
+  }
+
+  // Documentary Movies
+  fetchDocumentaryMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=99`);
+  }
+
+  // Science fiction movies
+  fetchScienceFictionMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=878`);
+  }
+
+  // Thriller movies
+  fetchThrillerMovies(): Observable<any> {
+    return this.http.get(`${this.baseurl}/discover/movie?api_key=${this.apikey}&with_genres=53`);
   }
 
 }
